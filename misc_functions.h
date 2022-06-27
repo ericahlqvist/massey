09bd8be3a879d1c2552c411b11ceb3e127e33cf7
Eric <ericahlq@gmail.com>
2022-06-27 09:56:33 +0200

    debugging

--- misc_functions.h
+++ misc_functions.h
@@ -77,8 +77,7 @@ int my_SQ_MAT_equal (GEN M1, GEN M2) {
             printf("loop2\n\n");
             if (!gequal(gel(gel(M1, i), j), gel(gel(M2, i), j))) {
                 printf("inside if\n\n");
-                output = 0;
-                break;
+                return = 0;
             }
         }
         
